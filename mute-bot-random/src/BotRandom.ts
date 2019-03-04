f37d858dd465658dd9a5c4a6eec94ad65a7144bb
Cédric Enclos <cedric.enclos@loria.fr>
2019-03-04 09:28:11 +0100

    fix(file): correct the filename in terminate function

--- mute-bot-random/src/BotRandom.ts
+++ mute-bot-random/src/BotRandom.ts
@@ -147,7 +147,7 @@ export class BotRandom {
       stats: new Stats(this.mutecore.state.sequenceCRDT).toString(),
     }
     console.log(data.stats)
-    await appendFileSync('./Logs.' + this.botname + ':' + this.network.id + '.json', ']')
+    await appendFileSync('./output/Logs.' + this.botname + ':' + this.network.id + '.json', ']')
     /* await writeFileSync(
       './Results.' + this.botname + ':' + this.network.id + '.json',
       JSON.stringify(data)
