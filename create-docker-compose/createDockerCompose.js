ff8b036a530b8f05512156d6d044dc31bd12eb04
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2020-06-26 09:33:03 +0200

    feat(create-docker-compose): simplify required config

--- create-docker-compose/createDockerCompose.js
+++ create-docker-compose/createDockerCompose.js
@@ -1,55 +1,45 @@
-var fs = require("fs");
+const fs = require("fs");
 
-var file = "version: '3.2'\nservices: \n";
-var conf = JSON.parse(fs.readFileSync("./config.json"));
+const conf = JSON.parse(fs.readFileSync("./config.json"));
+const nbOperationsPerBot = conf.objective / conf.nbBots
 
-var cptBot = 1;
-var master = "";
-var commandArgs = "";
+function generateCommand(index) {
+  const additionalArg = index !== 0 ? "-m ws://bot0:20001" : ""
+  const basicInfos = `-n Bot${index} ${additionalArg} --address bot${index} -p 20001 --delay ${conf.delay}`
 
-conf.bots.forEach(obj => {
-  if (obj.master) {
-    file += `  ${obj.botname.toLowerCase()}:\n`;
-    master = obj.botname;
-    commandArgs = "--address " + obj.botname.toLowerCase();
-  } else {
-    file += `  ${obj.botname.toLowerCase()}:\n`;
-    file += "    depends_on:\n" + `      - ${master.toLowerCase()}\n`;
-    commandArgs = "-m ws://master:20001 --address " + obj.botname.toLowerCase();
-    cptBot++;
-  }
+  const opsInfos = `-o ${conf.objective} --nb-operations ${nbOperationsPerBot} --operation-interval ${conf.operationInterval} --deletion ${conf.deletion} --move ${conf.move}`
+  const renameOpsInfos = conf.strategy === "rls" ?
+    `--nb-renaming-bots ${conf.nbRenamingBots} --rename-op-interval ${conf.renameOpInterval}` : ""
+  const crdtInfos = `--strategy ${conf.strategy} ${opsInfos} ${renameOpsInfos}`
 
-  file +=
-    "    image: botrandom:latest\n" +
-    '    user: "node"\n' +
-    "    volumes:\n" +
-    "      - type: bind\n" +
-    "        source: ../../Results/${BOTRANDOM_EXPERIMENT_NAME}\n" +
-    "        target: /home/node/app/output\n" +
-    "    deploy:\n" +
-    "      restart_policy:\n" +
-    "        condition: none\n" +
-    "    command:\n" +
-    "      - bash\n" +
-    "      - -c\n" +
-    "      - |\n" +
-    //"        npm i\n" +
-    "        npm start --" +
-    ` -n ${obj.botname}` +
-    ` ${commandArgs}` +
-    " -p 20001" +
-    ` -o ${conf.objective}` +
-    ` --delay ${conf.delay}` +
-    ` --time ${obj.time}` +
-    ` -s ${conf.snapshot}` +
-    ` --deletion ${obj.deletion}` +
-    ` --deplacement ${obj.move}` +
-    ` --operation ${obj.operation}` +
-    ` --strategy ${conf.strategy}` +
-    " && sync \n";
-});
+  const logInfos = `--snapshot ${conf.snapshot} --buffer-size ${conf.bufferSize} --log-interval ${conf.logInterval}`
 
-fs.writeFileSync(
-  "./docker-compose/docker-compose-" + Date.now() + ".yml",
-  file
-);
+  return `npm start -- ${basicInfos} ${crdtInfos} ${logInfos}`
+}
+
+function generateComposePerBot(index) {
+  return `  bot${index}:
+    image: botrandom:latest
+    user: 'node'
+    volumes:
+      - type: bind
+        source: ../../Results/\${BOTRANDOM_EXPERIMENT_NAME}
+        target: /home/node/app/output
+    deploy:
+      restart_policy:
+        condition: none
+    command:
+      - bash
+      - -c
+      - |
+        ${generateCommand(index)}
+`
+}
+
+const botsConfig = Array.from({ length: conf.nbBots }).map((_, index) => { return generateComposePerBot(index) }).join([""])
+
+const str = `version: '3.2'
+services:
+${botsConfig}`
+
+fs.writeFileSync(`./docker-compose/docker-compose-${Date.now()}.yml`, str)
